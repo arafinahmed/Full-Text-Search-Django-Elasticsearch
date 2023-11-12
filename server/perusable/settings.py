d0234fbb944822a8291fc78a49571d888e723b3a
Arafin Ahmed <arafin2021@gmail.com>
2023-11-12 14:46:46 +0600

    api setup

--- server/perusable/settings.py
+++ server/perusable/settings.py
@@ -41,7 +41,10 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'django_filters',
+]
 
 LOCAL_APPS = [
     'catalog.apps.CatalogConfig',
@@ -135,3 +138,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
